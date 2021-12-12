37f61c0eadcb90dab02894b21c363df694b46fbc
Lewis Davidson <lewisdavidsonznergi@gmail.com>
2021-12-12 19:50:37 +0000

    Blog Post page fetches data and renders image title and rich text content

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,52 @@
-import React from "react";
+import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export default function BlogPost() {
-  return <div></div>;
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: "blogPost",
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "blogPost",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { blogPost: items[0] },
+  };
+}
+
+export default function BlogPost({ blogPost }) {
+  const { featuredImage, title, content } = blogPost.fields;
+  return (
+    <div>
+      <div>
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h1>{title}</h1>
+      </div>
+      <div>{documentToReactComponents(content)}</div>
+    </div>
+  );
 }
