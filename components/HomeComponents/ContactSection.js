84de8ad03255e6370cf09ec7d9cd8c803758f743
Lewis Davidson <lewisdavidsonznergi@gmail.com>
2021-12-13 20:05:35 +0000

    Setup so form fields parse into json for body

--- components/HomeComponents/ContactSection.js
+++ components/HomeComponents/ContactSection.js
@@ -2,22 +2,30 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 
 const ContactSection = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit } = useForm();
+  async function onSubmit(e) {
+    e.preventDefault();
+    const formData = {};
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    fetch("api/mail", {
+      method: "post",
+      body: JSON.stringify(formData),
+    });
+    console.log(formData);
+  }
   return (
-    <div className="w-full min-h-screen">
-      <div className="grid md:grid-cols-2">
-        <div className="bg-primary w-full text-white font-bold p-4 sm:p-8 lg:p-32">
+    <div className="w-full">
+      <div className="grid items-center h-full md:grid-cols-2">
+        <div className="w-full h-full p-4 font-bold text-white bg-primary sm:p-8 lg:p-32">
           <h2 className="text-6xl lg:text-8xl">Let's Talk</h2>
-          <div className="pb-3 bg-white bg-no-repeat bg-left-bottom w-1/3 mb-2"></div>
+          <div className="w-1/3 pb-3 mb-2 bg-white bg-left-bottom bg-no-repeat"></div>
           <h3 className="text-4xl text-center mt-14">
             Find out what our experts can do
           </h3>
-          <p className="text-center mt-14 text-lg">
+          <p className="text-lg text-center mt-14">
             Fill out the form, drop us a call or shoot us an email. We’ll be in
             contact soon.
           </p>
@@ -28,19 +36,20 @@ const ContactSection = () => {
             className="mx-auto"
           ></img>
         </div>
-        <div className="flex items-center font-bold p-8">
+        <div className="flex items-center p-8 font-bold">
           <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="w-full md:w-3/4 mx-auto lg:grid grid-cols-2 gap-14 space-y-12 md:space-y-12 lg:space-y-0 lg:gap-24"
+            method="post"
+            onSubmit={onSubmit}
+            className="w-full grid-cols-2 mx-auto space-y-12 md:w-3/4 lg:grid gap-14 md:space-y-12 lg:space-y-0 lg:gap-24"
           >
-            {errors.name && <p className="text-red-600">This is required</p>}
+            {/* {errors.name && <p className="text-red-600">This is required</p>} */}
             <div className="relative border-b-2 focus-within:border-primary">
               <input
                 id="name"
                 name="name"
                 type="text"
                 placeholder=" "
-                className="block w-full appearance-none focus:outline-none bg-transparent"
+                className="block w-full bg-transparent appearance-none focus:outline-none"
                 {...register("name", { required: true })}
               ></input>
               <label
@@ -56,7 +65,7 @@ const ContactSection = () => {
                 name="email"
                 type="email"
                 placeholder=" "
-                className="block w-full appearance-none focus:outline-none bg-transparent"
+                className="block w-full bg-transparent appearance-none focus:outline-none"
                 {...register("email", { required: true })}
               ></input>
               <label
@@ -72,7 +81,7 @@ const ContactSection = () => {
                 name="tel"
                 type="tel"
                 placeholder=" "
-                className="block w-full appearance-none focus:outline-none bg-transparent"
+                className="block w-full bg-transparent appearance-none focus:outline-none"
                 {...register("tel")}
               ></input>
               <label
@@ -88,7 +97,7 @@ const ContactSection = () => {
                 name="companyName"
                 type="text"
                 placeholder=" "
-                className="block w-full appearance-none focus:outline-none bg-transparent"
+                className="block w-full bg-transparent appearance-none focus:outline-none"
                 {...register("companyName")}
               ></input>
               <label
@@ -104,7 +113,7 @@ const ContactSection = () => {
                 name="siteurl"
                 type="url"
                 placeholder=" "
-                className="block w-full appearance-none focus:outline-none bg-transparent"
+                className="block w-full bg-transparent appearance-none focus:outline-none"
                 {...register("siteurl")}
               ></input>
               <label
@@ -119,7 +128,7 @@ const ContactSection = () => {
                 id="message"
                 name="message"
                 placeholder=" "
-                className="block w-full appearance-none focus:outline-none bg-transparent"
+                className="block w-full bg-transparent appearance-none focus:outline-none"
                 {...register("message", { required: true })}
               ></textarea>
               <label
@@ -129,12 +138,12 @@ const ContactSection = () => {
                 Message *
               </label>
             </div>
-            <div className="bg-gradient-to-br from-primary to-secondary p-1 w-fit col-span-2">
+            <div className="col-span-2 p-1 bg-gradient-to-br from-primary to-secondary w-fit">
               <button
                 type="submit"
-                className="bg-white w-fit p-8 h-16 flex justify-center items-center rounded"
+                className="flex items-center justify-center h-16 p-8 bg-white rounded w-fit"
               >
-                <span className="bg-gradient-to-r from-primary to-secondary text-transparent bg-clip-text font-bold text-sm md:text-base lg:text-2xl mr-4">
+                <span className="mr-4 text-sm font-bold text-transparent bg-gradient-to-r from-primary to-secondary bg-clip-text md:text-base lg:text-2xl">
                   Send Message
                 </span>
                 <img src="/arrow-right.svg"></img>
--- components/ServicesPage.js
+++ components/ServicesPage.js
@@ -2,122 +2,133 @@ import { Lines } from "./HomeComponents/Lines";
 import { StepsCard, StepsFeaturesCard } from "./HomeComponents/StepsCard";
 
 export const Services = (props) => {
-    return (
-        <div>
-        <div className="relative w-full h-[550px]">
-            <Lines/>
+  return (
+    <div>
+      <div className="relative w-full h-[550px]">
+        <Lines />
+        <img
+          src={props.bannerImage}
+          alt={props.bannerAlt}
+          className="object-fill w-full h-full mx-auto"
+        />
+        <div className="absolute grid h-full grid-cols-4 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 min-w-section sm:px-10 all:px-4">
+          <div className="flex col-span-2 pb-16 my-auto">
+            <div className="w-[2px] ml-0 bg-primary"></div>
+            <div className="pl-4">
+              <span className="text-base font-bold text-primary font-rubik">
+                {props.preTitle /*Responsive*/}
+              </span>
+              <h1 className="pb-2 text-5xl font-bold text-white font-rubik">
+                {props.title /*Web Design*/}
+              </h1>
+              <p className="text-base text-white font-rubik">
+                {props.description}
+              </p>
+            </div>
+          </div>
+          <div className="relative w-full h-full text-right">
+            <p className="absolute right-2 bottom-0 mb-52 rotate-90 pl-2 origin-top-right font-rubik border-l-[2px] border-primary border-solid text-white text-sm font-bold z-10">
+              <a href={props.caseStudyLink}>
+                CASE STUDY:
+                <br />
+                {props.caseStudy}
+              </a>
+            </p>
+          </div>
+          <div>
             <img
-                src={props.bannerImage}
-                alt={props.bannerAlt}
-                className="mx-auto w-full h-full object-fill"
+              src={props.serviceImage}
+              alt={props.serviceImgAlt}
+              className="relative right-0 object-fill w-full mx-auto mt-16 z-2"
             />
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 grid grid-cols-4 min-w-section sm:px-10 all:px-4 h-full">
-                <div className="flex col-span-2 my-auto pb-16">
-                    <div className="w-[2px] ml-0 bg-primary"></div>
-                    <div className="pl-4">
-                        <span className="text-base text-primary font-rubik font-bold">{props.preTitle/*Responsive*/}</span>
-                        <h1 className="text-5xl text-white font-rubik font-bold pb-2">{props.title/*Web Design*/}</h1>
-                        <p className="text-base text-white font-rubik">{props.description}</p>
-                    </div>
-                </div>
-                <div className="relative text-right h-full w-full">
-                    <p className="absolute right-2 bottom-0 mb-52 rotate-90 pl-2 origin-top-right font-rubik border-l-[2px] border-primary border-solid text-white text-sm font-bold z-10"><a href={props.caseStudyLink}>CASE STUDY:<br/>{props.caseStudy}</a></p>
-                </div>
-                <div>                
-                    <img
-                        src={props.serviceImage}
-                        alt={props.serviceImgAlt}
-                        className="relative mx-auto w-full mt-16 object-fill right-0 z-2"
-                    />
-                    <img
-                        src={props.serviceOverlay}
-                        alt={props.serviceOverlayAlt}
-                        className="absolute right-6 top-12 translate-x-1/4 translate-y-1/3 z-100"
-                    />
-                </div>
-            </div>
+            <img
+              src={props.serviceOverlay}
+              alt={props.serviceOverlayAlt}
+              className="absolute right-6 top-12 translate-x-1/4 translate-y-1/3 z-100"
+            />
+          </div>
         </div>
-        <div className="relative -mt-20 max-w-section sm:px-10 all:px-4 h-[500px] mx-auto">
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-section mx-auto sm:px-10 all:px-4 grid grid-cols-4 h-[500px] w-screen">
-            <div className="relative h-[500px]">
-                <div className="absolute left-0 h-[500px] w-px bg-gray-200"></div>
-                <div className="absolute right-0 h-[500px] w-px bg-dash bg-dashGap"></div>
-            </div>
-            <div className="relative h-[500px]">
-                <div className="absolute right-0 h-[500px] w-px bg-dash bg-dashGap"></div>
-            </div>
-            <div className="relative h-[300px] pt-24">
-                <div className="absolute right-0 h-[300px] w-px bg-dash bg-dashGap"></div>
-            </div>
-            <div className="relative h-[500px]">
+      </div>
+      <div className="relative -mt-20 max-w-section sm:px-10 all:px-4 h-[500px] mx-auto">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-section mx-auto sm:px-10 all:px-4 grid grid-cols-4 h-[500px] w-screen">
+          <div className="relative h-[500px]">
+            <div className="absolute left-0 h-[500px] w-px bg-gray-200"></div>
+            <div className="absolute right-0 h-[500px] w-px bg-dash bg-dashGap"></div>
+          </div>
+          <div className="relative h-[500px]">
+            <div className="absolute right-0 h-[500px] w-px bg-dash bg-dashGap"></div>
+          </div>
+          <div className="relative h-[300px] pt-24">
+            <div className="absolute right-0 h-[300px] w-px bg-dash bg-dashGap"></div>
+          </div>
+          <div className="relative h-[500px]">
             <div className="absolute right-0 h-[500px] w-px bg-gray-200"></div>
-            </div>
-            </div>
-            <h3 className="relative text-center pt-40 text-black font-rubik font-bold text-2xl mx-auto">Who we've worked with</h3>
-            <div className="grid grid-cols-6 max-w-section h-[100px] sm:px-10 all:px-4 mt-10">
-                <img
-                    src="/client-logos/pgb.png"
-                    alt="Playground Bullys UK Logo"
-                    className="h-[100px] object-cover mx-auto"
-                />
-                <img
-                    src="/client-logos/mycabinbed.png"
-                    alt="My Cabin Bed Logo"
-                    className="h-[100px] object-cover mx-auto"
-                />
-                <img
-                    src="/client-logos/grandvenue.png"
-                    alt="Grand Venue Logo"
-                    className="h-[100px] object-cover mx-auto"
-                />
-                <img
-                    src="/client-logos/swapmyenergy.png"
-                    alt="Swap My Energy Logo"
-                    className="h-[100px] object-cover mx-auto"
-                />
-                <img
-                    src="/client-logos/unitecities.png"
-                    alt="Unite Cities Logo"
-                    className="h-[100px] object-cover mx-auto"
-                />
-                <img
-                    src="/client-logos/scs.png"
-                    alt="Specialist Coating Logo"
-                    className="h-[100px] object-cover mx-auto"
-                />
-            </div>
+          </div>
         </div>
-        <div className="bg-gray-200 h-screen w-full -skew-y-2">
-            <div className="max-w-section skew-y-2 mx-auto sm:px-10 all:px-4 pt-20">
-                <h3 className="font-bold font-rubik text-2xl">{props.title}</h3>
-                <div className="grid grid-cols-3 gap-2 h-[580px]">   >
-                    <StepsCard
-                        number="01"
-                        title="Scope Definition"
-                        description="We collaborate with the client to decide what objectives the new website must achieve. i.e., what is its purpose? We can determine the scope of the project once we know what the site's purpose is. I.e., what web pages and features the site will need to achieve the goal, as well as the timeframe for implementing them."                       
-                    />
-                    <StepsCard
-                        number="02"
-                        title="Wireframe Creation"
-                        description="Wireframes allow us to plan out how each page will be structured and how people will interact with the website."                       
-                    />
-                    <StepsCard
-                        number="04"
-                        title="Content Creation"  
-                        description="Now that we have a clearer picture of the site, we can begin creating content for individual pages, keeping in mind search engine optimization (SEO) to keep pages focused on a single topic."                     
-                    />
-                    <StepsCard
-                        number="05"
-                        title="Visual Elements"
-                        description="We begin working on the client's brand after we have the site architecture and some content in place. This allows us to define the visual aspect of the website."                       
-                    />
-                    <StepsCard
-                        number="06"
-                        title="Testing"                       
-                    />
-                </div>
-            </div>
+        <h3 className="relative pt-40 mx-auto text-2xl font-bold text-center text-black font-rubik">
+          Who we've worked with
+        </h3>
+        <div className="grid grid-cols-6 max-w-section h-[100px] sm:px-10 all:px-4 mt-10">
+          <img
+            src="/client-logos/pgb.png"
+            alt="Playground Bullys UK Logo"
+            className="h-[100px] object-cover mx-auto"
+          />
+          <img
+            src="/client-logos/mycabinbed.png"
+            alt="My Cabin Bed Logo"
+            className="h-[100px] object-cover mx-auto"
+          />
+          <img
+            src="/client-logos/grandvenue.png"
+            alt="Grand Venue Logo"
+            className="h-[100px] object-cover mx-auto"
+          />
+          <img
+            src="/client-logos/swapmyenergy.png"
+            alt="Swap My Energy Logo"
+            className="h-[100px] object-cover mx-auto"
+          />
+          <img
+            src="/client-logos/unitecities.png"
+            alt="Unite Cities Logo"
+            className="h-[100px] object-cover mx-auto"
+          />
+          <img
+            src="/client-logos/scs.png"
+            alt="Specialist Coating Logo"
+            className="h-[100px] object-cover mx-auto"
+          />
         </div>
+      </div>
+      <div className="w-full h-screen bg-gray-200 -skew-y-2">
+        <div className="pt-20 mx-auto max-w-section skew-y-2 sm:px-10 all:px-4">
+          <h3 className="text-2xl font-bold font-rubik">{props.title}</h3>
+          <div className="grid grid-cols-3 gap-2 h-[580px]">
+            <StepsCard
+              number="01"
+              title="Scope Definition"
+              description="We collaborate with the client to decide what objectives the new website must achieve. i.e., what is its purpose? We can determine the scope of the project once we know what the site's purpose is. I.e., what web pages and features the site will need to achieve the goal, as well as the timeframe for implementing them."
+            />
+            <StepsCard
+              number="02"
+              title="Wireframe Creation"
+              description="Wireframes allow us to plan out how each page will be structured and how people will interact with the website."
+            />
+            <StepsCard
+              number="04"
+              title="Content Creation"
+              description="Now that we have a clearer picture of the site, we can begin creating content for individual pages, keeping in mind search engine optimization (SEO) to keep pages focused on a single topic."
+            />
+            <StepsCard
+              number="05"
+              title="Visual Elements"
+              description="We begin working on the client's brand after we have the site architecture and some content in place. This allows us to define the visual aspect of the website."
+            />
+            <StepsCard number="06" title="Testing" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
